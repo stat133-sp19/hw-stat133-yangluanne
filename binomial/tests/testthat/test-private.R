61bbb41623141fef420e932779d47857e91d0da8
yangluanne <yangluanne@berkeley.edu>
2019-04-29 10:58:44 -0700

    included more tests

--- binomial/tests/testthat/test-private.R
+++ binomial/tests/testthat/test-private.R
@@ -4,32 +4,35 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("aux_mean works as expected", {
-  expect_equal(aux_mean(10, 0.3), 3)
-  expect_type(aux_mean(10, 0.3), 'double')
-  expect_length(aux_mean(10, 0.3), 1)
+test_that("check_prob works as expected", {
+  expect_true(check_prob(0.5))
+  expect_type(check_prob(0.5), 'logical')
+  expect_length(check_prob(0.5), 1)
 })
 
-test_that("aux_variance works as expected", {
-  expect_equal(aux_variance(10, 0.3), 2.1)
-  expect_type(aux_variance(10, 0.3), 'double')
-  expect_length(aux_variance(10, 0.3), 1)
+test_that("check_prob errors as expected", {
+  expect_error(check_prob(-1), 'invalid prob value')
+  expect_error(check_prob(2), 'invalid prob value')
 })
 
-test_that("aux_mode works as expected", {
-  expect_equal(aux_mode(10, 0.3), 3)
-  expect_type(aux_variance(10, 0.3), 'double')
-  expect_length(aux_variance(10, 0.3), 1)
+test_that("check_trials works as expected", {
+  expect_true(check_trials(1))
+  expect_type(check_trials(1), 'logical')
 })
 
-test_that('aux_skewness works as expected', {
-  expect_equal(round(aux_skewness(10, 0.3), 2), 0.28)
-  expect_type(aux_skewness(10, 0.3), 'double')
-  expect_length(aux_skewness(10, 0.3), 1)
+test_that("check_trials errors as expected", {
+  expect_error(check_trials(-1), 'invalid trials value')
 })
 
-test_that('aux_kurtosis works as expected', {
-  expect_equal(round(aux_kurtosis(10, 0.3), 2), -0.12)
-  expect_type(aux_kurtosis(10, 0.3), 'double')
-  expect_length(aux_kurtosis(10, 0.3), 1)
+test_that("check_success works as expected", {
+  expect_true(check_success(c(1, 2, 3), 4))
+  expect_type(check_success(c(1, 2, 3), 4), 'logical')
+})
+
+test_that("check_success errors as expect", {
+  expect_error(check_success(c(TRUE, FALSE), 2), 'invalid success value')
+  expect_error(check_success(c(), 2), 'invalid success value')
+  expect_error(check_success(c(1, 2, 3), -1), 'invalid trials value')
+  expect_error(check_success(c(-1, 2), 2), 'invalid success value')
+  expect_error(check_success(c(0.1, 2), 2), 'invalid success value')
 })
